28be5b95d6b579bd3dc4491296d6dce62572c1f8
Aaron Bushnell <aaron.bushnell@gmail.com>
2024-11-21 17:01:58 -0500

    Depict odd line-height

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
       <h1 className="hz">I do work (class: hz)</h1>
       <h1 className="hz1">I don't work (class: hz1)</h1>
       <h1 className="hz1z">I don't work (class: hz1z)</h1>
+      <h1 className="h-1">I work, but have odd line-height issues (class: h-1)</h1>
     </div>
   );
 }
